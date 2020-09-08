a59363c73eecf672c3b5bfb78e820e9fd471de16
rfminguez <rfminguez@gmail.com>
2020-09-08 02:53:50 +0200

    parte 03 analisis interactivo

--- src/toolbox.py
+++ src/toolbox.py
@@ -1,2 +1,14 @@
-def test_toolbox():
-    print("Test toolbox")
+def unique_sorted_values_plus_ALL(array):
+    '''
+    receives: a list of elements.
+    returns: a list of unique elements plus the value "ALL"
+
+    code from:
+    https://towardsdatascience.com/bring-your-jupyter-notebook-to-life-with-interactive-widgets-bc12e03f0916
+    '''
+    ALL = 'ALL'
+    unique = array.unique().tolist()
+    unique.sort()
+    unique.insert(0, ALL)
+    return unique
+
